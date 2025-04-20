930d3a947223bc2f700358215e0e36f59855a06d
Hubab777 <159882654+Hubab777@users.noreply.github.com>
2025-04-20 19:29:01 +0500

    Updated unit-converter.py

--- unit_converter_app/unit-converter.py
+++ unit_converter_app/unit-converter.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Unit Converter App")
-st.markdown("### Instant Converter for Length, Weight and Time")
-st.write("Welcome! Choose a category, enter the value and get real-time converted result")
+st.title("Unit Converter App ✨⚖️")
+st.markdown("### Instant Converter for Length📏, Weight⚖️ and Time⏲️")
+st.write("Welcome! Choose a category, enter the value and get real-time converted result 🚀")
 
-category = st.selectbox("Select any category", ["Length", "Weight", "Time"])
+category = st.selectbox("Select any category ✅", ["Length", "Weight", "Time"])
 
 def convert_units(category, value, unit):
     if category == "Length":
@@ -34,16 +34,16 @@ def convert_units(category, value, unit):
             return value * 24
         
 if category == "Length":
-    unit = st.selectbox("Select Conversion",["Kilometres to Miles", "Miles to Kilometres"])
+    unit = st.selectbox("Select Conversion 🔄",["Kilometres to Miles", "Miles to Kilometres"])
         
 elif category == "Weight":
-    unit = st.selectbox("Select Conversion", ["Kilograms to Pounds", "Pounds to Kilograms"])
+    unit = st.selectbox("Select Conversion 🔄", ["Kilograms to Pounds", "Pounds to Kilograms"])
     
 elif category == "Time":
-    unit = st.selectbox("Select Conversion", ["Seconds to Minutes", "Minutes to Seconds", "Minutes to Hours", "Hours to Minutes", "Hours to Days", "Days to Hours"])
+    unit = st.selectbox("Select Conversion 🔄", ["Seconds to Minutes", "Minutes to Seconds", "Minutes to Hours", "Hours to Minutes", "Hours to Days", "Days to Hours"])
     
-value = st.number_input("Enter the value")
+value = st.number_input("Enter the value 💬")
 
-if st.button("Convert"):
+if st.button("Convert 🏁"):
     result = convert_units(category, value, unit)
     st.success(f"The result is {result:.2f}")
